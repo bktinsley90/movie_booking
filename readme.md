241b7a234d1e2d8217d35ea74a5218c3b437df2f
bktinsley90 <bktinsley90@gmail.com>
2024-04-09 20:07:25 -0400

    added local torage to save data

---
+++ readme.md
@@ -0,0 +1,7 @@
+#Movie Booking
+
+## Description
+
+This is a simple movie booking application. It allows users to book tickets for movies. The application has two main entities: movies and users. Users can view the list of movies and book tickets for them. The application also allows users to view their booking history.
+
+![Movie Booking](./assets/movie-booking.png)
--- script.js
+++ script.js
@@ -1,29 +1,57 @@
 const container = document.querySelector('.container');
-const seats= document.querySelectorAll('.row .seat:not(.occupied)');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
-
+populateUI();
 let ticketPrice = +movieSelect.value;
 
-function updateSelectedCount(){
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
   const selectedSeatsCount = selectedSeats.length;
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
 }
+
+// Get data from local storage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    selectedSeats.forEach((seat) => {
+      seats[seat].classList.add('selected');
+    });
+  }
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
 // movie select event updateing the ticket price
-movieSelect.addEventListener('change', e => {
+movieSelect.addEventListener('change', (e) => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
-
-
 // seat click event
-container.addEventListener('click', e => {
-  if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
+container.addEventListener('click', (e) => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
     e.target.classList.toggle('selected');
     updateSelectedCount();
   }
 });
+
+// Initial count and total set
+updateSelectedCount();
